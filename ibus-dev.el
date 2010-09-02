c428c7f1607e1db4dd759cbc73d5ee4306af515c
IRIE Shinsuke <irieshinsuke@yahoo.co.jp>
2010-09-03 05:34:05 +0900

    Minor change (Modified comment)

--- ibus-dev.el
+++ ibus-dev.el
@@ -8,7 +8,7 @@
 ;; Maintainer: S. Irie
 ;; Keywords: Input Method, i18n
 
-(defconst ibus-mode-version "0.2.0.4")
+(defconst ibus-mode-version "0.2.0.5")
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -2247,7 +2247,7 @@ respectively."
 	    (if simulate
 		(setq this-command 'self-insert-command))
 	    (insert-and-inherit text))
-	   ;; Normal commit (Undoing will be performed every 20 characters)
+	   ;; Normal commit (Undoing will be performed every 20 columns)
 	   (t
 	    (if simulate
 		(setq this-command 'self-insert-command))
